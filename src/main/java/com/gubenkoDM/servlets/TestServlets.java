d1d328cff1a918f2df8c4ca7aec857beb6dcc0db
nestora103 <nestora103@gmail.com>
2016-12-19 16:52:44 +0300

    First commit
    First commit

---
+++ src/main/java/com/gubenkoDM/servlets/TestServlets.java
@@ -0,0 +1,11 @@
+package com.gubenkoDM.servlets;
+
+/**
+ * Created by DmitriX on 19.12.2016.
+ */
+public class TestServlets {
+    public static void main(String[] args) {
+        MainServlet mServlet=new MainServlet();
+
+    }
+}
